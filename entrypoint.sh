d215db05fe534688ec97dea9ff9e83b4c82a085e
hadar-co <hadar@datree.io>
2022-02-07 10:44:21 +0200

    add gif example

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,12 @@
 
 inputfile="$INPUT_FILE"
 options="$INPUT_OPTIONS"
-token=""
 
-if [ -n "$INPUT_TOKEN" ]; then
-    token="$INPUT_TOKEN"
-elif [ -n "$DATREE_TOKEN" ]; then
-    token="$DATREE_TOKEN"
-else
+if [ -z "$DATREE_TOKEN" ]; then
     echo "No account token configured, see https://github.com/datreeio/action-datree for instructions"
     exit 1
 fi
 
 curl https://get.datree.io | /bin/bash
-datree config set token "$token"
+
 datree test $inputfile $options
