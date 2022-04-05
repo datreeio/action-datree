9ed51128da87b53967e0072f6549caa701eb6790
hadar-co <hadar@datree.io>
2022-04-05 17:29:51 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,23 @@
-#!/bin/sh -l
+#!/bin/sh
 
 inputpath="$INPUT_PATH"
 cliArguments="$INPUT_CLIARGUMENTS"
 isHelmChart="$INPUT_ISHELMCHART"
 helmArgs="$INPUT_HELMARGUMENTS"
+isKustomization="$INPUT_ISKUSTOMIZATION"
+kustomizeArgs="$INPUT_KUSTOMIZEARGUMENTS"
+
 
 if [ -z "$DATREE_TOKEN" ]; then
     echo "No account token configured, see https://github.com/datreeio/action-datree for instructions"
     exit 1
 fi
 
-curl https://get.datree.io | /bin/bash
-
 if [ "$isHelmChart" = "true" ]; then
-    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | /bin/bash
-    helm plugin install https://github.com/datreeio/helm-datree
-    
     helm datree test $inputpath $cliArguments -- $helmArgs
+elif [ "$isKustomization" = "true" ]; then
+    datree kustomize test $inputpath $cliArguments -- $kustomizeArgs
+    
 else
     datree test $inputpath $cliArguments  
 fi
