c5e8c37d9e250371ea2819352eba627e8bd5a2a7
hadar-co <hadar@datree.io>
2022-11-06 14:42:21 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -70,6 +70,7 @@ function create_report() {
     echo "### Failed rules:" >>"$GITHUB_STEP_SUMMARY"
     echo "" >>"$GITHUB_STEP_SUMMARY"
   else
+    echo "$GITHUB_STEP_SUMMARY" > "/datreeSummary.md"
     echo "### 🥳 All rules passed successfully! 🥳" >>"$GITHUB_STEP_SUMMARY"
     return
   fi
