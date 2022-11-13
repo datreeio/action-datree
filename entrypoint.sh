bd11f142f483d3abe2028cfe3a16a9cea874972e
hadar-co <hadar@datree.io>
2022-11-13 11:25:11 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -80,7 +80,7 @@ function create_report() {
       echo "**>> Filename: $FILENAME**" >> "$GITHUB_STEP_SUMMARY"
       for ((j = 0; j < $(jq ".k8sValidationResults[$i].errors | length" "$RESULT_JSON_PATH"); j++)); do
         ERROR=$(jq ".k8sValidationResults[$i].errors[$j].ErrorMessage" "$RESULT_JSON_PATH")
-        echo "❌ $ERROR" >> "$GITHUB_STEP_SUMMARY"
+        echo "❌ k8s schema validation error: $ERROR" >> "$GITHUB_STEP_SUMMARY"
       done
     done
 
