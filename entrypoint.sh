635338c78e0885b438a8e62964b36afb8eb2689e
hadar-co <hadar@datree.io>
2022-11-23 11:51:26 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -167,8 +167,7 @@ elif [ "$isKustomization" = "true" ]; then
   create_report "$inputpath"
 else
   if [[ "$cliArguments" == *"sarif"* ]]; then
-    datree test $inputpath $cliArguments > "$HOME"/output.sarif
-    ls -al "$HOME"
+    datree test $inputpath $cliArguments > output.sarif
   else
     datree test $inputpath $cliArguments
   fi
